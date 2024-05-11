c4325ed0aabef0fb5b9049cba0544fa725b1caa5
AndriyV2504 <154068131+AndriyV2504@users.noreply.github.com>
2024-05-11 15:00:23 +0300

    add-task-9-storedata

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,14 @@
-const formData = { email: "", message: "" };
+let formData = { email: "", message: "" };
+
+document.querySelector(".feedback-form").addEventListener("input", function(event) {
+    formData[event.target.name] = event.target.value.trim();
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+});
+
+const storeData = localStorage.getItem("feedback-form-state");
+if (storeData) {
+    formData = JSON.parse(storeData);
+    document.querySelector('input[name = "email"]').value = formData.email;
+    document.querySelector('textarea[name = "message"]').value = formData.message;
+};
 
