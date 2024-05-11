a28c92702566db4123c777af75624f5a8a09de65
AndriyV2504 <154068131+AndriyV2504@users.noreply.github.com>
2024-05-11 15:21:09 +0300

    add-task-9-prevent

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -12,3 +12,15 @@ if (storeData) {
     document.querySelector('textarea[name = "message"]').value = formData.message;
 };
 
+document.querySelector(".feedback-form").addEventListener("submit", function(event) {
+    if (formData.email === "" || formData.message === "") {
+        alert("Fill please all fields");
+    } else {
+        console.log(formData);
+        localStorage.removeItem("feedback-form-state");
+        formData = { email: "", message: "" };
+        document.querySelector('input[name = "email"]').value = "";
+        document.querySelector('textarea[name = "message"]').value = "";
+    }
+    event.preventDefault();
+});
